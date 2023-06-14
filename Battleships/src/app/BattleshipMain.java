421160791483310eaf48ce21719f0b1711add13a
jCoyleWHS <jason.coyle@studmail.w-hs.de>
2023-06-14 15:58:00 +0200

    Erstellung Grundprojekt

---
+++ Battleships/src/app/BattleshipMain.java
@@ -0,0 +1,9 @@
+package app;
+
+public class BattleshipMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Battleships/src/module-info.java
+++ Battleships/src/module-info.java
@@ -3,7 +3,9 @@
  */
 /**
  * @author jason
+ *  --module-path "D:\javafx-sdk-20.0.1\lib" --add-modules javafx.controls,javafx.fxml
  *
  */
 module Battleships {
+	requires javafx.controls;
 }
